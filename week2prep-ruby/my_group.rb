c17236e9c8f02d3a60a687aae44917fb96e3438a
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-04 18:16:19 +0200

    saving changes

---
+++ week2prep-ruby/my_group.rb
@@ -0,0 +1,10 @@
+irb(main):005:0> my_group = Array.new
+=> []
+irb(main):006:0> person_1 = {name:"Olof",gender:"female", age: 27}
+=> {:name=>"Olof", :gender=>"female", :age=>27}
+irb(main):007:0> person_2 = {name:"Gudjon", gender:"male", age: 26}
+=> {:name=>"Gudjon", :gender=>"male", :age=>26}
+irb(main):008:0> person_3 = {name:"Anna", gender:"female", age: 47}
+=> {:name=>"Anna", :gender=>"female", :age=>47}
+irb(main):009:0> my_group = [person_1, person_2, person_3]
+=> [{:name=>"Olof", :gender=>"female", :age=>27}, {:name=>"Gudjon", :gender=>"male", :age=>26}, {:name=>"Anna", :gender=>"female", :age=>47}]
