289fcbf9ebd415ce610afd764a461e7e8198daef
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-06 19:53:15 +0200

    99 bottles of beer

--- week2prep-ruby/flow.rb
+++ week2prep-ruby/flow.rb
@@ -1,8 +1,22 @@
-command = gets
-
-while command != 'bye'
-  puts command
-  command = gets.chomp
+bottles = 99
+bottle = bottles-1
+while bottles >= 0
+  if bottles > 1
+puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer'
+puts 'Take one down and pass it around, '
+puts bottle.to_s  + ' bottles of beer'
+puts
 end
+    if bottles == 1
+  puts '1 bottle of beer on the wall'
+  puts '1 bottle of beer'
+  puts ''
+    end
 
-puts 'Come again soon!'
+  if bottles == 0
+    puts 'No more bottles of beer on the wall, no more bottles of beer.'
+    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+  end
+bottles = bottles - 1
+bottle = bottles -1
+end
