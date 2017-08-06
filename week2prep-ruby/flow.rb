1b1f1219d8b7888b3995181c68f8061d7b563b62
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-06 20:06:36 +0200

    grandma

--- week2prep-ruby/flow.rb
+++ week2prep-ruby/flow.rb
@@ -1,22 +1,15 @@
-bottles = 99
-bottle = bottles-1
-while bottles >= 0
-  if bottles > 1
-puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer'
-puts 'Take one down and pass it around, '
-puts bottle.to_s  + ' bottles of beer'
-puts
-end
-    if bottles == 1
-  puts '1 bottle of beer on the wall'
-  puts '1 bottle of beer'
-  puts ''
-    end
+puts 'Nice of you to visit grandma!'
+input = gets.chomp
 
-  if bottles == 0
-    puts 'No more bottles of beer on the wall, no more bottles of beer.'
-    puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
-  end
-bottles = bottles - 1
-bottle = bottles -1
+while input !='BYE'
+
+  if input == input.upcase
+    puts 'NO NOT SINCE ' + (1930 +rand(17)).to_s + '!'
+
+  else
+    puts 'HUH?! SPEAK UP, SONNY'
+
+  end 
+  input = gets.chomp
 end
+puts 'OK. BYE!'
