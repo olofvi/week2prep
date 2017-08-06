116b1e143b0ac9a0107ac2f96767bf0e0758d70c
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-06 17:55:01 +0200

    flow pr.6

--- week2prep-ruby/flow.rb
+++ week2prep-ruby/flow.rb
@@ -1,6 +1,8 @@
-puts 'Hello, what\'s your name?'
+puts 'I am a fortune-teller.  Tell me your name:'
 name = gets.chomp
-puts 'Hello, ' + name + '.'
 if name == 'Olof'
-  puts 'What a lovely name!'
+  puts 'I see great things in your future.'
+else
+  puts 'Your future is... Oh my!  Look at the time!'
+  puts 'I really have to go, sorry!'
 end
